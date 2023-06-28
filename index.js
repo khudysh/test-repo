f1b8edec0b088eb0f13decfdafb592fb52a8c018
Eugene Khudyshkin <60621694+khudysh@users.noreply.github.com>
2023-06-28 16:16:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let gg = 7;
 // let res = $.ajax(url, { method: 'POST', dataType: 'json', })
 // console.log(res)
 
-let mas = [999999999999]
+let mas = [999, 999, 999]
 
 // $.ajax({ 
 //     type: 'POST', 
